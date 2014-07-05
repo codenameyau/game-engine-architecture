ae86e3820664c963e047e9ae02ef5b5d15474d0b
Jorge Yau <jyau01@citymail.cuny.edu>
2014-07-04 22:07:19 -0400

    Adding core engine header

--- src/lib/core.cpp
+++ src/lib/core.cpp
@@ -5,54 +5,59 @@
  * Contains core game engine components
  */
 
+#include <cstdio>
+#include "core.h"
 
-/*************************
- * Core - Scene Renderer *
- *************************/
-class CoreRenderer
-{
-public:
-
-  // Constructor
-  CoreRenderer();
-
-  // Destructor
-  ~CoreRenderer();
-
-  // Properties
-  void startUp() {
-
-  }
-
-  // Methods
-  void shutDown() {
-
-  }
-
-};
+namespace ENGINE_CORE {
 
 
 /*************************
  * Core - Memory Manager *
  *************************/
-class CoreMemory
-{
-public:
 
   // Constructor
-  CoreMemory();
+  MemoryManager::MemoryManager() {};
 
   // Destructor
-  ~CoreMemory();
+  MemoryManager::~MemoryManager() {};
 
   // Properties
-  void startUp() {
 
+
+  // Public Methods
+  void MemoryManager::startUp() {
+    printf("[+] Starting memory manager\n");
   }
 
-  // Methods
-  void shutDown() {
-
+  void MemoryManager::shutDown() {
+    printf("[-] Shutting down memory manager\n");
   }
 
-};
+
+/*************************
+ * Core - Scene Renderer *
+ *************************/
+
+  // Constructor
+  Renderer::Renderer() {};
+
+  // Destructor
+  Renderer::~Renderer() {};
+
+  // Properties
+
+
+  // Public Methods
+  void Renderer::startUp() {
+    printf("[+] Starting engine renderer\n");
+  }
+
+  void Renderer::shutDown() {
+    printf("[-] Shutting down engine renderer\n");
+  }
+
+
+
+
+
+} // end ENGINE_CORE
--- src/lib/core.h
+++ src/lib/core.h
@@ -1,5 +1,54 @@
-#ifndef CORE_H
-#define CORE_H
+#ifndef ENGINE_CORE_H
+#define ENGINE_CORE_H
 
+namespace ENGINE_CORE {
+
+
+/*************************
+ * Core - Memory Manager *
+ *************************/
+class MemoryManager
+{
+public:
+
+  // Constructor
+  MemoryManager();
+
+  // Destructor
+  ~MemoryManager();
+
+  // Properties
+  void startUp();
+
+  // Methods
+  void shutDown();
+
+};
+
+
+/*************************
+ * Core - Scene Renderer *
+ *************************/
+class Renderer
+{
+public:
+
+  // Constructor
+  Renderer();
+
+  // Destructor
+  ~Renderer();
+
+  // Properties
+  void startUp();
+
+  // Methods
+  void shutDown();
+
+};
+
+
+
+} // end ENGINE_CORE
 
 #endif
